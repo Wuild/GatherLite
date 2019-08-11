4192a8d9948dc5fa0efe96bc1717544612d45e43
Wuild <wuildz@gmail.com>
2019-08-12 01:10:49 +0200

    Added localization and minimap button

---
+++ Scripts/localization.lua
@@ -0,0 +1,59 @@
+-- @TODO: Fix these translations
+
+local name, GatherLite = ...
+
+GatherLite.locale = {};
+
+-- English translations
+GatherLite.locale.CAT_ORE = "Ore"
+GatherLite.locale.CAT_HERB = "Herb"
+GatherLite.locale.CAT_TREASURE = "Treasure";
+GatherLite.locale.ORE_CLASS_VEIN = "Vein"
+GatherLite.locale.ORE_CLASS_DEPOSIT = "Deposit"
+
+GatherLite.locale.ORE_COPPER = "copper"
+GatherLite.locale.ORE_TIN = "tin"
+GatherLite.locale.ORE_IRON = "iron"
+GatherLite.locale.ORE_SILVER = "silver"
+GatherLite.locale.ORE_TRUESILVER = "truesilver"
+GatherLite.locale.ORE_GOLD = "gold"
+GatherLite.locale.ORE_MITHRIL = "mithril"
+GatherLite.locale.ORE_THORIUM = "thorium"
+GatherLite.locale.ORE_RTHORIUM = "thorium (rich)"
+GatherLite.locale.ORE_DARKIRON = "dark iron"
+
+GatherLite.locale.HERB_ARTHASTEAR = "arthas' tears"
+GatherLite.locale.HERB_BLACKLOTUS = "black lotus"
+GatherLite.locale.HERB_BLINDWEED = "blindweed"
+GatherLite.locale.HERB_BRIARTHORN = "briarthorn"
+GatherLite.locale.HERB_BRUISEWEED = "bruiseweed"
+GatherLite.locale.HERB_DREAMFOIL = "dreamfoil"
+GatherLite.locale.HERB_EARTHROOT = "earthroot"
+GatherLite.locale.HERB_FADELEAF = "fadeleaf"
+GatherLite.locale.HERB_FIREBLOOM = "firebloom"
+GatherLite.locale.HERB_GHOSTMUSHROOM = "ghost mushroom"
+GatherLite.locale.HERB_GOLDENSANSAM = "golden sansam"
+GatherLite.locale.HERB_GOLDTHORN = "goldthorn"
+GatherLite.locale.HERB_GRAVEMOSS = "grave moss"
+GatherLite.locale.HERB_GROMSBLOOD = "gromsblood"
+GatherLite.locale.HERB_ICECAP = "icecap"
+GatherLite.locale.HERB_KHADGARSWHISKER = "khadgar's whisker"
+GatherLite.locale.HERB_KINGSBLOOD = "kingsblood"
+GatherLite.locale.HERB_LIFEROOT = "liferoot"
+GatherLite.locale.HERB_MAGEROYAL = "mageroyal"
+GatherLite.locale.HERB_MOUNTAINSILVERSAGE = "mountain silversage"
+GatherLite.locale.HERB_PEACEBLOOM = "peacebloom"
+GatherLite.locale.HERB_PLAGUEBLOOM = "plaguebloom"
+GatherLite.locale.HERB_PURPLELOTUS = "purple lotus"
+GatherLite.locale.HERB_SILVERLEAF = "silverleaf"
+GatherLite.locale.HERB_STRANGLEKELP = "stranglekelp"
+GatherLite.locale.HERB_SUNGRASS = "sungrass"
+GatherLite.locale.HERB_SWIFTTHISTLE = "swiftthistle"
+GatherLite.locale.HERB_WILDSTEELBLOOM = "wild steelbloom"
+GatherLite.locale.HERB_WINTERSBITE = "wintersbite"
+GatherLite.locale.HERB_WILDVINE = "wildvine"
+
+-- @TODO: Translate to french
+-- @TODO: Translate to german
+-- @TODO: Translate to chinese
+-- @TODO: Translate to russian
