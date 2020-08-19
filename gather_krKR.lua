40366ae8beb225bdb8c6a34c5eb7757ede20ee9c
yelst <69845731+yelst@users.noreply.github.com>
2020-08-20 00:08:41 +0900

    Korean locale
    little localization for korean

---
+++ gather_krKR.lua
@@ -0,0 +1,63 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("GatherLite", "krKR")
+if not L then
+    return
+end
+
+L["tracking"] = "추적"
+L["mining"] = "채광"
+L["herbalism"] = "약초"
+L["fish"] = "낚시"
+L["containers"] = "상자"
+
+L["worldmap.show"] = "GatherLite 표시"
+L["worldmap.hide"] = "GatherLite 숨기기"
+
+-- 채광mining
+L['node.copper_vein'] = "구리 광맥"
+L['node.tin_vein'] = "주석 광맥"
+L['node.silver_vein'] = "은 광맥"
+L['node.iron_deposit'] = "철 광맥"
+L['node.gold_vein'] = "금 광맥"
+L['node.mithril_deposit'] = "미스릴 광맥"
+L['node.truesilver_deposit'] = "진은 광맥"
+L['node.dark_iron_deposit'] = "검은무쇠 광맥"
+L['node.small_thorium_vein'] = "작은 토륨 광맥"
+L['node.rich_thorium_vein'] = "풍부한 토륨 광맥"
+
+-- 약초herbalism
+L['node.silverleaf'] = "은옆수잎"
+L['node.peacebloom'] = "평온초"
+L['node.earthroot'] = "뱀뿌리"
+L['node.mageroyal'] = "마법초"
+L['node.briarthorn'] = "찔레가시"
+L['node.stranglekelp'] = "갈래물풀"
+L['node.bruiseweed'] = "생채기풀"
+L['node.wild_steelbloom'] = "야생 철쭉"
+L['node.grave_moss'] = "무덤이끼"
+L['node.kingsblood'] = "왕꽃잎풀"
+L['node.liferoot'] = "생명의 뿌리"
+L['node.fadeleaf'] = "미명초"
+L['node.goldthorn'] = "황금가시"
+L['node.khadgars_whisker'] = "카드가의 수염"
+L['node.wintersbite'] = "겨울서리풀"
+L['node.firebloom'] = "화염초"
+L['node.purple_lotus'] = "보라 연꽃"
+L['node.arthas_tears'] = "아서스의 눈물"
+L['node.sungrass'] = "태양풀"
+L['node.blindweed'] = "실명초"
+L['node.ghost_mushroom'] = "유령버섯"
+L['node.gromsblood'] = "그롬의 피"
+L['node.golden_sansam'] = "황금 산삼"
+L['node.dreamfoil'] = "꿈풀"
+L['node.mountain_silversage'] = "은초롱이"
+L['node.plaguebloom'] = "역병초"
+L['node.icecap'] = "얼음송이"
+L['node.black_lotus'] = "검은 연꽃"
+
+-- 상자containers
+L['node.battered_chest'] = "낡은 궤짝"
+L['node.large_battered_chest'] = "낡은 대형 궤짝"
+L['node.solid_chest'] = "단단한 궤짝"
+L['node.large_solid_chest'] = "크고 단단한 궤짝"
+L['node.giant_clam'] = "대합"
+L['node.ungoro_dirt_pile'] = "운고로 토양"
