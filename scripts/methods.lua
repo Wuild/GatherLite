9bef9bbfe103d547f2ddb09335c65d034c3d1c7e
mks-mud <mks-github@mud.hu>
2023-03-28 01:56:22 +0200

    Got the following error when I've attempted to loot a titanium ore in Wintergrasp: Gatherlite/scripts/methods.lua:274: attempt to perform arithmetic on field 'count' (a nil value)

--- scripts/methods.lua
+++ scripts/methods.lua
@@ -265,6 +265,9 @@ function GatherLite:UpdateNode(type, nodeID, mapID, posX, posY)
             if node.loot[exists].count == nil then
                 node.loot[exists].count = 0;
             end
+            if item.count == nil then
+                item.count = 0;
+            end
 
             node.loot[exists].count = node.loot[exists].count + item.count;
         end
